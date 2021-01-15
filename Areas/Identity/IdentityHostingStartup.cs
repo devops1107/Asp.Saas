47f8f8b558181d7fd4cdb1630a1c4847ee417f68
Administrator <monte4952@gmail.com>
2021-01-16 03:02:42 +0800

    initial

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Asp.Saas.Areas.Identity.IdentityHostingStartup))]
+namespace Asp.Saas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
